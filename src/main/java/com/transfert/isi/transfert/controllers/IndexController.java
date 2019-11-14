aba6028dce90522b76bf061c9ef971795c2876cb
Abakar Fils Mahamat <mahamatabakarmht@gmail.com>
2019-11-14 11:58:17 +0000

    ok

--- src/main/java/com/transfert/isi/transfert/controllers/IndexController.java
+++ src/main/java/com/transfert/isi/transfert/controllers/IndexController.java
@@ -1,4 +1,11 @@
 package com.transfert.isi.transfert.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class IndexController {
+
+    public String index() {
+        return "index";
+    }
 }
